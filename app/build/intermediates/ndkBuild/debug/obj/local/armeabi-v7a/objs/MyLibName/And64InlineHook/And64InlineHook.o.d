a107873d0d667524da176b87a93ed81e6311c949
makput01 <jokokendil4410@gmail.com>
2025-01-19 15:33:33 +0000

    tes

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs/MyLibName/And64InlineHook/And64InlineHook.o.d
@@ -0,0 +1,20 @@
+/workspaces/tes/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs/MyLibName/And64InlineHook/And64InlineHook.o: \
+  /workspaces/tes/app/src/main/jni/And64InlineHook/And64InlineHook.cpp \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/inttypes.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__config \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstring \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/errno.h
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/inttypes.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__config:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/errno.h:
---
+++ app/build/intermediates/ndkBuild/release/obj/local/armeabi-v7a/objs/MyLibName/Main.o.d
@@ -0,0 +1,248 @@
+/workspaces/tes/app/build/intermediates/ndkBuild/release/obj/local/armeabi-v7a/objs/MyLibName/Main.o: \
+  /workspaces/tes/app/src/main/jni/Main.cpp \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/list \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__config \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/memory \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/version \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/exception \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdint.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/new \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/utility \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstring \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__debug \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/limits \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iterator \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/tuple \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/atomic \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__threading_support \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/chrono \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ctime \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ratio \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/climits \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/limits.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/errno.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/functional \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/bit \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/vector \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__bit_reference \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__split_buffer \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/fstream \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ostream \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ios \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__locale \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string_view \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__string \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cctype \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/wctype.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/mutex \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__mutex_base \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/system_error \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__errc \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cerrno \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/locale.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/support/xlocale/__posix_l_fallback.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/streambuf \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/locale \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdarg \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/bitset \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/istream \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/filesystem \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stack \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/deque \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iomanip \
+  /workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iostream \
+  /workspaces/tes/app/src/main/jni/Includes/Logger.h \
+  /workspaces/tes/app/src/main/jni/Includes/obfuscate.h \
+  /workspaces/tes/app/src/main/jni/Includes/Utils.h \
+  /workspaces/tes/app/src/main/jni/KittyMemory/MemoryPatch.h \
+  /workspaces/tes/app/src/main/jni/KittyMemory/KittyUtils.h \
+  /workspaces/tes/app/src/main/jni/KittyMemory/KittyMemory.h \
+  /workspaces/tes/app/src/main/jni/Menu.h \
+  /workspaces/tes/app/src/main/jni/Substrate/SubstrateHook.h \
+  /workspaces/tes/app/src/main/jni/Substrate/CydiaSubstrate.h
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/list:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__config:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/memory:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/type_traits:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstddef:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/version:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/exception:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdint:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/new:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/utility:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__tuple:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__debug:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/limits:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iterator:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/tuple:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/atomic:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/chrono:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ctime:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ratio:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/climits:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/limits.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/algorithm:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/functional:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/bit:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/vector:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__bit_reference:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__split_buffer:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/fstream:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ostream:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ios:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__locale:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/string_view:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__string:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdio:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cwchar:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cwctype:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cctype:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/mutex:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__mutex_base:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/system_error:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__errc:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cerrno:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/locale.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/support/xlocale/__posix_l_fallback.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/streambuf:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/locale:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/cstdarg:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/bitset:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/istream:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/filesystem:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/stack:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/deque:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iomanip:
+
+/workspaces/android-sdk/ndk/22.0.7026061/sources/cxx-stl/llvm-libc++/include/iostream:
+
+/workspaces/tes/app/src/main/jni/Includes/Logger.h:
+
+/workspaces/tes/app/src/main/jni/Includes/obfuscate.h:
+
+/workspaces/tes/app/src/main/jni/Includes/Utils.h:
+
+/workspaces/tes/app/src/main/jni/KittyMemory/MemoryPatch.h:
+
+/workspaces/tes/app/src/main/jni/KittyMemory/KittyUtils.h:
+
+/workspaces/tes/app/src/main/jni/KittyMemory/KittyMemory.h:
+
+/workspaces/tes/app/src/main/jni/Menu.h:
+
+/workspaces/tes/app/src/main/jni/Substrate/SubstrateHook.h:
+
+/workspaces/tes/app/src/main/jni/Substrate/CydiaSubstrate.h:
