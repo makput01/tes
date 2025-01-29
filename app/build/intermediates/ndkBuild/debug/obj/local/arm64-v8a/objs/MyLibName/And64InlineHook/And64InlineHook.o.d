8c496126856942a15c0527f5d7d61760b4f898f4
makput01 <jokokendil4410@gmail.com>
2025-01-29 14:37:53 +0700

    tes

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs/MyLibName/And64InlineHook/And64InlineHook.o.d
@@ -0,0 +1,23 @@
+C:/Users/DELL/AndroidStudioProjects/fsd/app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs/MyLibName/And64InlineHook/And64InlineHook.o: \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\And64InlineHook\And64InlineHook.cpp \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\inttypes.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\And64InlineHook\And64InlineHook.hpp
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\inttypes.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h:
+
+C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\And64InlineHook\And64InlineHook.hpp:
---
+++ app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs/MyLibName/Substrate/SubstrateHook.o.d
@@ -0,0 +1,152 @@
+C:/Users/DELL/AndroidStudioProjects/fsd/app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs/MyLibName/Substrate/SubstrateHook.o: \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\SubstrateHook.cpp \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\CydiaSubstrate.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\SubstrateDebug.hpp \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\SubstrateLog.hpp \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Includes\obfuscate.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string_view \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__string \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\utility \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\version \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__debug \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\memory \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\exception \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\new \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\limits \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\iterator \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\tuple \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\atomic \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\chrono \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\ctime \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\ratio \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\climits \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\functional \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\bit \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cctype \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h \
+  C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h
+
+C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\CydiaSubstrate.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\SubstrateDebug.hpp:
+
+C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Substrate\SubstrateLog.hpp:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+
+C:\Users\DELL\AndroidStudioProjects\fsd\app\src\main\jni\Includes\obfuscate.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\string_view:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__string:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\utility:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\version:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__debug:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\memory:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\exception:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\new:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\limits:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\iterator:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\tuple:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\atomic:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\chrono:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\ctime:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\ratio:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\climits:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\functional:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\bit:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\cctype:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h:
+
+C:\SDK\ndk\22.0.7026061\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h:
